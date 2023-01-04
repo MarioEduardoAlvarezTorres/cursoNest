06b1328fffc0c76464502e286b1327ead8d20207
MarioEduardo <eduardoat44@gmail.com>
2023-01-04 15:08:09 -0600

    Inyeccion de dependencias

---
+++ Seccion2 - TypeScript/typescript-intro/src/api/pokeApi.adapter.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { PokeapiResponse } from '../interfaces/pokeapi-response.interface';
+
+export class PokeApiAdapter{
+    private readonly axios = axios;
+    
+    async get(url:string){
+        const { data } = await axios.get<PokeapiResponse>(url);
+        return data;
+    }
+
+    async post(url:string, data:any){
+        
+    }
+
+    async patch(url:string, data:any){
+        
+    }
+
+    async delete(url:string){
+        
+    }
+}
--- Seccion2 - TypeScript/typescript-intro/src/main.ts
+++ Seccion2 - TypeScript/typescript-intro/src/main.ts
@@ -1,7 +1,5 @@
-import './style.css'
-// import { name, age , isValid } from './bases/01-types'
-// import { bulbasaur } from './bases/02-objects'
-import { charmander } from './bases/03-class'
+import { charmander } from "./bases/04-injection"
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>Hello Vite ${charmander.name} !!!</h1>
 `
