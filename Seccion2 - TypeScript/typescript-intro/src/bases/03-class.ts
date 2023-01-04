edf3bf6c71e3e454da300377eb91343788443295
MarioEduardo <eduardoat44@gmail.com>
2023-01-04 14:20:08 -0600

    Getters,metodos y THIS

--- Seccion2 - TypeScript/typescript-intro/src/bases/03-class.ts
+++ Seccion2 - TypeScript/typescript-intro/src/bases/03-class.ts
@@ -9,10 +9,27 @@ export class Pokemon {
     // }
     // Es lo mismo de arriba pero en corto
     
+
+    get imgUrl():string{
+
+        return `https://pokemon.com/${this.id}.jpg`; //El this apunta a la instacia
+    }
     constructor(
-        public id:number,
-        public name:string
+        public readonly id:number,
+        public name:string,
+        // public imageUrl:string,
     ){}
+
+    scream(){
+        console.log(`${this.name.toUpperCase()} !!!`)
+    }
+
+    speak(){
+        console.log(`${this.name , this.name}`)
+    }
 }
 
-export const charmander = new Pokemon(1,'Pikachu')
+export const charmander = new Pokemon(1,'charmander')
+console.log(charmander)
+charmander.scream()
+charmander.speak()
