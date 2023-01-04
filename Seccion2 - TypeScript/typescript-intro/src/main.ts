30b808965c89f018203321a3c420db054afc3e86
MarioEduardo <eduardoat44@gmail.com>
2023-01-04 13:58:26 -0600

    Clases y formas abreviadas

--- Seccion2 - TypeScript/typescript-intro/src/main.ts
+++ Seccion2 - TypeScript/typescript-intro/src/main.ts
@@ -1,8 +1,8 @@
 import './style.css'
 // import { name, age , isValid } from './bases/01-types'
 // import { bulbasaur } from './bases/02-objects'
-
+import { charmander } from './bases/03-class'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>Hello Vite !!!</h1>
+  <h1>Hello Vite ${charmander.name} !!!</h1>
 `
 
