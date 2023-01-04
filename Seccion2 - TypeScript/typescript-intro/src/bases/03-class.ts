12c7b91db05d4387dafcbffd43a3bdce59e2e9ea
MarioEduardo <eduardoat44@gmail.com>
2023-01-04 14:44:51 -0600

    Colocar tipo de dato a respuestas http genericos

--- Seccion2 - TypeScript/typescript-intro/src/bases/03-class.ts
+++ Seccion2 - TypeScript/typescript-intro/src/bases/03-class.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Move, PokeapiResponse } from '../interfaces/pokeapi-response.interface';
 export class Pokemon {
     // public id: number;
     // public name:string;
@@ -29,9 +30,11 @@ export class Pokemon {
         console.log(`${this.name , this.name}`)
     }
 
-    async getMoves(){
-        const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon/4')
-        console.log(data.moves)
+    async getMoves(): Promise<Move[]> {
+        
+        const {data} = await axios.get<PokeapiResponse>('https://pokeapi.co/api/v2/pokemon/4')
+        console.log(data.moves[0])
+
         return data.moves
     }
 }
