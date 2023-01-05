fe1806656428b2fdfc46b88d2f967b743c0a606d
MarioEduardo <eduardoat44@gmail.com>
2023-01-04 21:09:36 -0600

    Cambio de nombre de carpetas

--- Seccion2 - TypeScript/typescript-intro/src/main.ts
+++ Seccion2 - TypeScript/typescript-intro/src/main.ts
@@ -1,6 +1,12 @@
-import {charmander} from './bases/05-decorators'
+import { charmander } from './bases/06-decorators2';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>Hello Vite ${charmander.name} !!!</h1>
-`
+import './style.css';
 
+
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `
+  <h1>Hello ${ charmander.name } ${ charmander.id }!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
